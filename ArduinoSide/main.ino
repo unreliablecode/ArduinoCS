41cef8c97fe6c29d93318464e2f24aab78520fe4
Crystal Oscillator <78525180+unreliablecode@users.noreply.github.com>
2023-08-16 13:09:28 +0700

    Update main.ino

--- ArduinoSide/main.ino
+++ ArduinoSide/main.ino
@@ -1,9 +1,34 @@
-void setup()
-{
-  
+#include <ArduinoJson.h>
+
+struct ArduinoMessage {
+    int GPIO;
+    int LOGIC;
+    float Value;
+    int isDigitalWrite;
+    char Message_[50];
+};
+
+void setup() {
+    Serial.begin(9600);
 }
 
-void loop()
-{
-  
+void loop() {
+    if (Serial.available() > 0) {
+        String jsonStr = Serial.readStringUntil('\n');
+        DynamicJsonDocument jsonDoc(256);
+        deserializeJson(jsonDoc, jsonStr);
+
+        int gpio = jsonDoc["GPIO"];
+        int logic = jsonDoc["LOGIC"];
+        float value = jsonDoc["Value"];
+        int isDigital = jsonDoc["isDigitalWrite"];
+        if (isDigital == 1)
+        {
+            DigitalWrite(gpio, logic);
+        }
+        else if (isDigitalWrite == 0)
+        {
+            AnalogWrite(gpio, value);
+        }
+    }
 }
